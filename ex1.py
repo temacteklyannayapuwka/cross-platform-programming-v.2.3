ca26588fd0daf76e27fb8876e5dc8d334e5cef4e
Artem Bakanov <rtmbakanov@gmail.com>
2023-06-09 21:50:35 +0300

    add ex1.py

---
+++ ex1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    s = input("Put sentence: ")
+    r = s.replace(" ", "_")
+    print("Sentence after substitutions: {0}".format(r))
+    input("Press any button to exit")
